3b1ba7c2c4df50d61ca00a3d3e75e5ca9a34d867
Manthankumar Satani <satanimanthan@gmail.com>
2022-11-22 11:40:05 +0530

    feat: scroll on right sub menu and next page to top

--- app.vue
+++ app.vue
@@ -1,3 +1,7 @@
+<script setup>
+import "~/assets/css/main.css";
+</script>
+
 <template>
   <AppLayout>
     <NuxtPage />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 export default defineNuxtConfig({
-  extends: "@nuxt-themes/docus",
+  extends: ["@nuxt-themes/docus"],
   ssr: false,
+  nitro: {
+    esbuild: {
+      options: {
+        exclude: [],
+      },
+    },
+  },
   content: {
     highlight: {
       // theme: {
-      //   // Default theme (same as single string)
+      // Default theme (same as single string)
       //   default: "github-light",
-      //   // Theme used if `html.dark`
+      // Theme used if `html.dark`
       //   dark: "github-dark",
-      //   // Theme used if `html.sepia`
+      // Theme used if `html.sepia`
       //   sepia: "monokai",
       // },
       preload: ["js", "json", "solidity"],
