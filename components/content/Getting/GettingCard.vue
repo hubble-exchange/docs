86acd8be82b007b0143a7077dd425791b6da6a9f
Dhaval Purohit <dhavalpurohit.work@gmail.com>
2022-12-27 22:19:17 +0530

    fix: mobile view and folder structure

--- components/content/Getting/GettingCard.vue
+++ components/content/Getting/GettingCard.vue
@@ -22,14 +22,20 @@ defineProps({
 
 <template>
   <NuxtLink :class="{ blurry }" class="card" :to="to">
-    <div class="w-full flex h-full items-center p-5">
-      <div class="title text-base">
-        <ContentSlot :use="$slots.linkTitle" unwrap="p">
+    <div class="w-full flex items-center mb-20">
+      <h3 class="title text-6 md:text-10">
+        <ContentSlot :use="$slots.title" unwrap="p">
           Card title
         </ContentSlot>
-      </div>
+      </h3>
       <Icon class="ml-auto" :name="icon || 'uit:arrow-up-right'" />
     </div>
+    <slot />
+    <p class="description">
+      <ContentSlot :use="$slots.description" unwrap="p">
+        Card description
+      </ContentSlot>
+    </p>
   </NuxtLink>
 </template>
 
@@ -39,15 +45,10 @@ css({
     display: 'block',
     position: 'relative',
     width: '100%',
-    maxHeight:'{space.16}',
-    padding: '{space.1}',
+    padding: '{space.7}',
     borderRadius: '{radii.xl}',
     border: '1px solid {color.gray.200}',
     background: '{backdrop.background}',
-    marginBottom:'{space.4}',
-    '&:last-child':{
-        marginBottom:'{space.0}',
-    },
     '@dark': {
       borderColor: '{color.gray.900}',
       color: '{color.gray.50}'
@@ -57,14 +58,26 @@ css({
     },
     '.icon': {
       display: 'inline-block',
-      width: '{space.6}',
-      height: '{space.6}'
+      width: '{space.8}',
+      height: '{space.8}',
+      '@md': {
+        width: '{space.12}',
+        height: '{space.12}',
+      }
     },
     '.title': {
 
       lineHeight: '{text.lg.lineHeight}',
       fontWeight: '{fontWeight.semibold}'
     },
+    '.description': {
+      fontSize: '{text.xl.fontSize}',
+      lineHeight: '{text.sm.lineHeight}',
+      fontWeight: '{fontWeight.medium}',
+      '@md': {
+        fontSize: '{text.3xl.fontSize}',
+      }
+    }
   }
 })
 </style>
--- components/content/Links/LinkCard.vue
+++ components/content/Links/LinkCard.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 defineProps({
+  title: {
+    type: String,
+    default: 'Card title',
+  },
   to: {
     type: String,
     default: '/blogs',
   },
-  icon: {
+  target: {
     type: String,
-    default: '',
+    default: undefined,
   },
-  iconClass: {
+  icon: {
     type: String,
     default: '',
   },
@@ -21,22 +25,22 @@ defineProps({
 </script>
 
 <template>
-  <NuxtLink :class="{ blurry }" class="card" :to="to">
-    <div class="w-full flex items-center mb-20">
-      <h3 class="title text-10">
-        <ContentSlot :use="$slots.title" unwrap="p">
-          Card title
-        </ContentSlot>
-      </h3>
+  <NuxtLink v-if="!target" :class="{ blurry }" class="card" :to="to">
+    <div class="w-full flex h-full items-center p-5">
+      <div class="title text-base">
+        {{ title }}
+      </div>
       <Icon class="ml-auto" :name="icon || 'uit:arrow-up-right'" />
     </div>
-    <slot />
-    <p class="description">
-      <ContentSlot :use="$slots.description" unwrap="p">
-        Card description
-      </ContentSlot>
-    </p>
   </NuxtLink>
+  <a v-else :class="{ blurry }" class="card" :href="to" :target="target">
+    <div class="w-full flex h-full items-center p-5">
+      <div class="title text-base">
+        {{ title }}
+      </div>
+      <Icon class="ml-auto" :name="icon || 'uit:arrow-up-right'" />
+    </div>
+  </a>
 </template>
 
 <style scoped lang="ts">
@@ -45,10 +49,15 @@ css({
     display: 'block',
     position: 'relative',
     width: '100%',
-    padding: '{space.7}',
+    maxHeight:'{space.16}',
+    padding: '{space.1}',
     borderRadius: '{radii.xl}',
     border: '1px solid {color.gray.200}',
     background: '{backdrop.background}',
+    marginBottom:'{space.4}',
+    '&:last-child':{
+        marginBottom:'{space.0}',
+    },
     '@dark': {
       borderColor: '{color.gray.900}',
       color: '{color.gray.50}'
@@ -58,19 +67,14 @@ css({
     },
     '.icon': {
       display: 'inline-block',
-      width: '{space.12}',
-      height: '{space.12}'
+      width: '{space.6}',
+      height: '{space.6}'
     },
     '.title': {
 
       lineHeight: '{text.lg.lineHeight}',
       fontWeight: '{fontWeight.semibold}'
     },
-    '.description': {
-      fontSize: '{text.3xl.fontSize}',
-      lineHeight: '{text.sm.lineHeight}',
-      fontWeight: '{fontWeight.medium}',
-    }
   }
 })
 </style>
